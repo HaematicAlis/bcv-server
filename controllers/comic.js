85c601eb52c7e60d23f8e7f32c71628edfb22ba8
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-07-14 17:26:38 -0400

    swapped link to album

--- controllers/comic.js
+++ controllers/comic.js
@@ -13,7 +13,7 @@ export const getComics = async (req, res) => {
 
 export const addComic = async (req, res) => {
     const comicInfo = req.body;
-    const newComic = new Comic({ name: comicInfo.name, owner: comicInfo.owner, link: comicInfo.album });
+    const newComic = new Comic({ name: comicInfo.name, owner: comicInfo.owner, album: comicInfo.album });
 
     try {
         await newComic.save();
