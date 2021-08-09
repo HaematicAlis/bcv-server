8da70e13a26b69cbb0213c1b0b4a1cb4d953ec5e
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2021-08-09 15:41:28 -0400

    endpoints for setting comics/images as done

--- controllers/comic.js
+++ controllers/comic.js
@@ -35,14 +35,43 @@ export const deleteComic = async (req, res) => {
     }
 }
 
-export const setDone = async (req, res) => {
+export const setComicDone = async (req, res) => {
     const id = req.body.id;
     const done = req.body.done;
 
     try {
+        console.log('----------------------');
+        console.log('id: ' + id + '   done: ' + done);
+        console.log('finding comic...');
         const comic = await Comic.findById(id);
+        console.log('found the comic:');
+        console.log(comic.done);
+        console.log('setting comic done to ' + done);
         comic.done = done;
+        console.log('saving comic...');
         await comic.save();
+        res.status(201).json(comic);
+    } catch (error) {
+        res.status(409).json({ message: error });
+    }
+}
+
+export const setDone = async (req, res) => {
+    const id = req.body.id;
+    const index = req.body.index;
+    const done = req.body.done;
+
+    try {
+        console.log('----------------------');
+        console.log('id: ' + id + '   index: ' + index + '   done: ' + done);
+        console.log('finding comic...');
+        const comic = await Comic.findById(id);
+        console.log('found the comic.');
+        console.log('getting image at index ' + index + ' and setting done to ' + done);
+        comic.images[index].done = done;
+        console.log('saving comic...');
+        await comic.save();
+        console.log('success');
         res.status(201).json(comic);
     } catch (error) {
         res.status(409).json({ message: error });
--- routes/comic.js
+++ routes/comic.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { getComics, addComic, deleteComic } from '../controllers/comic.js';
+import { getComics, addComic, deleteComic, setDone, setComicDone } from '../controllers/comic.js';
 
 const comicRouter = express.Router();
 
 comicRouter.post('/get', getComics);
 comicRouter.post('/add', addComic);
 comicRouter.post('/delete', deleteComic);
+comicRouter.post('/setDone', setDone);
+comicRouter.post('/setComicDone', setComicDone);
 
 export default comicRouter;
